01e601ae49f78496f5495e740c3cc3bdfd7bb25c
Rohith Kona <50866037+Rohith0007@users.noreply.github.com>
2024-07-31 14:49:39 -0400

    Update asgi.py

--- controller/ModelPredict/asgi.py
+++ controller/ModelPredict/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelPredict.settings')
+settings_module = 'ModelPredict.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ModelPredict.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODUL', settings_module)
 
 application = get_asgi_application()
